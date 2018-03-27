ce969fd51b8798f260c3800a518a923c3274ca60
Martin Skarzynski <marskar@gmail.com>
2018-03-26 21:00:08 -0400

    join function is working

--- kod/join.R
+++ kod/join.R
@@ -1,3 +1,20 @@
-adult <- readr::read_csv("../dat/adult.csv")
-mort <- readr::read_csv("../dat/mort.csv")
+adult <- readr::read_csv("dat/adult.csv")
+mort <- readr::read_csv("dat/mort.csv")
+library('dplyr')
+amor <- left_join(x = adult, y = mort, by = "SEQN")
+dim(amor)
 
+amor2 <- amor %>%
+    filter(HAC1N==2 & HAC1O==2) %>%
+    mutate(canc_mort = if_else(UCOD_LEADING=='002', true = 1, false = 0))
+table(amor2$canc_mort)
+
+min(amor2$HSAITMOR)
+all(amor2$HAC1N==2)
+all(amor2$HAC1O==2)
+dim(amor2)
+names(amor2)
+unique(amor2$UCOD_LEADING)
+canc_mort <- amor2 %>%
+    filter(UCOD_LEADING=='002')
+dim(canc_mort)
