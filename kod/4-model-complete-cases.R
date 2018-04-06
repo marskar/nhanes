b6cddf16a115ae40ff217f5336819c87fcd5ad19
Martin Skarzynski <marskar@gmail.com>
2018-04-06 19:17:12 -0400

    finished working on my R script that trains cox models with and without ridge and lasso penalties

--- kod/4-model-complete-cases.R
+++ kod/4-model-complete-cases.R
@@ -1,3 +1,7 @@
+#!/usr/local/bin Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
 library(here)
 library(readr)
 library(dplyr)
@@ -5,10 +9,7 @@ library(survey)
 source(here("kod/frailty.controldf.R"))
 source(here("kod/lasso.R"))
 
-seed <- sample(seq(99), size = 1)
-set.seed(seed = seed)
-
-
+set.seed(seed = args[1])
 #move PERMTH_INT and canc_mort to the beginning
 #sample a tenth of the dataset columns
 samp <- read_rds(here('dat/3-clean-complete-cases.rds')) %>%
@@ -19,7 +20,7 @@ samp <- read_rds(here('dat/3-clean-complete-cases.rds')) %>%
            SDPSTRA6,
            WTPFQX6,
            everything()[sample(seq(ncol(.)),
-                               size=ncol(.)/10)])
+                               size=round(ncol(.)/10))])
 
 # create survey design object
 des <- svydesign(ids = ~SDPPSU6, strata = ~SDPSTRA6, weights = ~WTPFQX6, nest = TRUE, data = samp)
@@ -31,35 +32,46 @@ vrs <- as.name(paste(names(samp)[3:ncol(samp)],
 vrs2 <- as.name(paste(names(samp)[3:ncol(samp)],
                       collapse=', '))
 
+set.seed(seed = args[1])
 train <- sample(x = seq(nrow(samp)),
-               size = nrow(samp)*.7)
-
-coxfit <- update(form, paste("~ ", vrs)) %>%
-svycoxph(design = des, data = samp[train,])
-
-coxmse <- mean(x = coxfit$residuals^2)
-coxmse
-aic <- AIC(coxfit)[2]
-pred <- survfit(coxfit, newdata = samp[-train,])
-predict(object = coxfit, newdata = samp[-train,])
-coxfitsumm <- summary(coxfit)
-
-prediction_train<-predict(basic.coxph,train,type="risk")
-
-prediction_test<-predict(object=basic.coxph,newdata=validate,type="risk")
-
+               size = round(nrow(samp)*.7))
+seed = args[1]
 # generate cox models without and with penalties
 update(form, paste("~ ", vrs)) %>%
 svycoxph(design = des, data = samp) %>%
-write_rds(paste0("obj/cox-model", seed, ".rds"))
+write_rds(here(paste0("obj/",
+                 round(AIC(.)[2]),
+                 "-",
+                 round(summary(.)$concordance[1]*100),
+                 "-",
+                 "coxmodel",
+                 "-",
+                 seed,
+                 ".rds")))
 
 update(form, paste("~ ridge(", vrs2, ')')) %>%
 svycoxph(design = des, data = samp) %>%
-write_rds(paste0("obj/ridge-model", seed, ".rds"))
+write_rds(here(paste0("obj/",
+                 round(AIC(.)[2]),
+                 "-",
+                 round(summary(.)$concordance[1]*100),
+                 "-",
+                 "ridmodel",
+                 "-",
+                 seed,
+                 ".rds")))
 
 update(form, paste("~ lasso(", vrs2, ')')) %>%
 svycoxph(design = des, data = samp) %>%
-write_rds(paste0("obj/lasso-model", seed, ".rds"))
+write_rds(here(paste0("obj/",
+                 round(AIC(.)[2]),
+                 "-",
+                 round(summary(.)$concordance[1]*100),
+                 "-",
+                 "lasmodel",
+                 "-",
+                 seed,
+                 ".rds")))
 
 #update(form, paste("~ ridge(", vrs2, ', theta = 10 )'))
 #update(form, paste("~ lasso(", vrs2, ', theta = 10 )'))
