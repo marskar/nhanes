e9824692523e3c2668fe280fb7f7159396f86f1a
Martin Skarzynski <marskar@gmail.com>
2018-04-12 16:25:16 -0400

    ready to train cox and rid models

--- 5-remodel-complete-cases.R
+++ 5-remodel-complete-cases.R
@@ -7,10 +7,10 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(survey)
-source(here("kod/frailty.controldf.R"))
-source(here("kod/lasso.R"))
+library(purrr)
 
-#set.seed(seed = args[1])
+# obtain seed from command line argument
+set.seed(seed = args[1])
 #move PERMTH_INT and canc_mort to the beginning
 #sample a tenth of the dataset columns
 read_rds(here('dat/3-clean-complete-cases.rds')) %>%
@@ -31,6 +31,7 @@ svydesign(ids = ~SDPPSU6,
           nest = TRUE,
           data = samp) ->
 des
+
 # create left side of equations
 form <- as.formula(Surv(PERMTH_INT, canc_mort) ~ x1)
 # create right sides of equations
@@ -42,77 +43,48 @@ vrs2 <- as.name(paste(names(samp)[6:ncol(samp)],
 set.seed(seed = args[1])
 train <- sample(x = seq(nrow(samp)),
                size = round(nrow(samp)*.7))
-seed = args[1]
 # generate cox models without and with penalties
 
 cox <- svycoxph(update(form,
                        paste("~ ", vrs)),
-                design = des, data = samp)
+                design = des, data = samp[train,])
 
 rid <- svycoxph(update(form,
                        paste("~ ridge(", vrs2, ')')),
-                design = des, data = samp)
+                design = des, data = samp[train,])
 
-las <-  svycoxph(update(form,
-                       paste("~ lasso(", vrs2, ')')),
-                design = des, data = samp)
+# define functions needed to create first table
+get_con <- function(x) {
+signif(summary(x)$concordance[1]*100, digits = 2)
+}
+get_HR <- function(x) {
+summary(x)$conf.int[,"exp(coef)"]
+}
+get_HR_CI_lower <- function(x) {
+summary(x)$conf.int[,"lower .95"]
+}
+get_HR_CI_upper <- function(x) {
+summary(x)$conf.int[,"upper .95"]
+}
+get_coef_pvalue <- function(x) {
+    coefs <- summary(x)$coef
+    coefs[,ncol(coefs)]
+}
+model_list <- list(cox, rid)
+seed = args[1]
 
-get_con = function(x) round(summary(x)$concordance[1]*100)
+data_frame(seed = rep(seed,3),
+           type = c('coxph', 'ridge', 'lasso'),
+           aic = AIC(cox, rid, las)[,"AIC"],
+           concordance =  map_dbl(model_list,
+                                  get_con),
+           hazard_ratio = map(model_list,
+                              get_HR),
+           HR_CI_lower =  map(model_list,
+                              get_HR_CI_lower),
+           HR_CI_upper =  map(model_list,
+                              get_HR_CI_upper),
+           coef_pvalue =  map(model_list,
+                             get_coef_pvalue)) %>%
+write_rds(here(paste0("obj/", seed, ".rds")))
 
-seed=1
-
-data_frame(type = c('coxph', 'ridge', 'lasso'),
-           seed = rep(seed,3),
-           aic = AIC(cox, rid, las)[,2],
-           concordance = c(get_con(cox),
-                           get_con(rid),
-                           get_con(las)),
-           betas = c(list(coef(cox)),
-                     list(coef(rid)),
-                     list(coef(las)))) ->
-df
-
-%>%
-write_rds(here(paste0("obj/",
-                 seed,
-                 ".rds")))
-library('lime')
-signif(summary(cox)$coef[,1], digits=2)
-signif(summary(cox)$coef[,2], digits=2)
-
-summary(cox)$wald
-
-function(x){
-  x <- summary(x)
-  pval<-signif(x$wald["pvalue"],
-                  digits=2)
-  wald<-signif(x$wald["test"],
-                    digits=2)
-  beta<-signif(x$coef[,1],
-               digits=2);#coeficient beta
-  HR <-signif(x$coef[,2],
-              digits=2);#exp(beta)
-  HR_CI_lower <- signif(x$conf.int[,"lower .95"], 2)
-  HR_CI_upper <- signif(x$conf.int[,"upper .95"],2)
-  HR_CI <- paste0(HR,
-               " (",
-               HR_CI_lower,
-               "-",
-               HR_CI_upper,
-               ")")
-  res<-c(beta,
-         HR,
-         wald,
-         pval)
-  names(res)<-c("beta",
-                "HR (95% CI for HR)",
-                "wald test", 
-                "p value")
-  return(res) } ->
-get_modstats
-  #return(exp(cbind(coef(x),confint(x))))
-#
-#signif(summary(cox)$wald["pvalue"], digits=2)
-#summary(cox)$p.value
-#update(form, paste("~ ridge(", vrs2, ', theta = 10 )'))
-#update(form, paste("~ lasso(", vrs2, ', theta = 10 )'))
