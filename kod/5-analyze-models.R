bf3adac243a4a66860acea00991de3cbce214eb2
Martin Skarzynski <marskar@gmail.com>
2018-04-12 18:40:24 -0400

    ready to analyze coefs

--- kod/5-analyze-models.R
+++ kod/5-analyze-models.R
@@ -3,23 +3,21 @@ library(here)
 library(dplyr)
 library(ggplot2)
 
-dat <- read_csv(here("dat/model-data.csv"),
-                col_names = FALSE,
-                col_types = "iici") %>%
-                rename(aic = X1,
-                       concord = X2,
-                       type = X3,
-                       seed = X4)
+dat <- read_rds(here("dat/4-model-complete-cases.rds"))
+dat %>%
+    head
 
 dat %>%
-    ggplot(aes(x = concord,
-               y = aic)) +
+    ggplot(aes(x = aic,
+               y = concordance)) +
            geom_point() +
            theme_minimal()
 
 best_df <- dat %>%
-    filter(concord > 80,
-           aic  < 14480)
+    filter(concordance > 80,
+           aic  < 14460)
+best_df
+
 best_coef
 
 best_mod <- read_rds(here('obj/14452-81-ridmodel-56.rds'))
