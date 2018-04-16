7825c29195f0c98cc7f331e7352deb5aecb2fb9a
Martin Skarzynski <marskar@gmail.com>
2018-04-16 00:01:06 -0400

    going to get stats on 9600 models

--- 6-model-diff-sizes.R
+++ 6-model-diff-sizes.R
@@ -1,11 +1,12 @@
 #!/usr/local/bin Rscript
-installed.packages("furrr")
 library(here)
 library(readr)
 library(dplyr)
 library(tidyr)
 library(survey)
 library(purrr)
+library(furrr)
+plan(multiprocess)
 
 # this function takes in two integers as an argument
 # this function returns a dataframe
@@ -84,15 +85,15 @@ get_modelstats <- function(seed, size){
                size = size,
                type = c('coxph', 'ridge'),
                aic = AIC(cox, rid)[,"AIC"],
-               concordance =  map_dbl(model_list,
+               concordance =  future_map_dbl(model_list,
                                       get_con),
-               hazard_ratio = map(model_list,
+               hazard_ratio = future_map(model_list,
                                   get_HR),
-               HR_CI_lower =  map(model_list,
+               HR_CI_lower =  future_map(model_list,
                                   get_HR_CI_lower),
-               HR_CI_upper =  map(model_list,
+               HR_CI_upper =  future_map(model_list,
                                   get_HR_CI_upper),
-               coef_pvalue =  map(model_list,
+               coef_pvalue =  future_map(model_list,
                                  get_coef_pvalue))
 }
 
@@ -103,9 +104,10 @@ map2_dfr(.x = seed,
          .y = seq(48),
          get_modelstats)
 }
-map_dfr(seq(10), map_sizes) %>%
+future_map_dfr(seq(100), map_sizes) %>%
 write_rds(here(paste0("dat/6-model-diff-sizes.rds")))
 
 df <- read_rds(here("dat/6-model-diff-sizes.rds"))
-df
+df  %>%
+    tail
 all(!is.na(df))
